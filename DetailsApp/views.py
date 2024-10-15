6ecf8076e823f0d9530430ff9b01d8ae16e0952d
Charan-1100 <chinnukstar143@Gmail.com>
2024-10-15 22:37:16 +0530

    "Updated pycache files, modified views.py, and changed db.sqlite3"

--- DetailsApp/views.py
+++ DetailsApp/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url='login')
+# @login_required(login_url='login')
 def index(request):
     return render(request,'index.html')
 
