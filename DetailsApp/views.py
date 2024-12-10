8b47a3a4a4ba1f74aeed3ed8d50cc0dc7625d068
Charan-1100 <chinnukstar143@Gmail.com>
2024-12-10 16:20:04 +0530

    `Added HttpResponse in views.py and modified return statements in Data function. Updated binary files in __pycache__ directory.`

--- DetailsApp/views.py
+++ DetailsApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -60,7 +60,7 @@ def Data(request):
      else:
         P='Please Fill The Form'
    
-    return render()
+    return HttpResponse("Saved Sucessfull")
 
 def log_out(request):
     logout(request)
