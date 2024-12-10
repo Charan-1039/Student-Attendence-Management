ca28ad175aa64906b2908e23a9eb40c601829c19
Charan-1100 <chinnukstar143@Gmail.com>
2024-12-10 14:42:26 +0530

    `Updated StudentApp/models.py to import Semester and added Department field to Students model`

--- StudentApp/models.py
+++ StudentApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from DetailsApp.models import Department
+from DetailsApp.models import Department,Semester
 
 # Create your models here.
 class Students(models.Model):
   StudentName= models.CharField(max_length=255)
   USN=models.CharField(max_length=255)
-  Sem=models.ForeignKey(Department,on_delete=models.CASCADE ,null=True)
+  Sem=models.ForeignKey(Semester,on_delete=models.CASCADE ,null=True)
+  Department=models.ForeignKey(Department,on_delete=models.CASCADE ,null=True)
 
 def __str__(self) :
         return self.StudentName
