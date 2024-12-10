363c4d8825279999940b80f3be72470b69ad616f
Charan-1100 <chinnukstar143@Gmail.com>
2024-12-10 15:06:03 +0530

    `Added custom admin interfaces for models in DetailsApp, FacultyApp and StudentApp`

--- DetailsApp/admin.py
+++ DetailsApp/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from DetailsApp.models import Department,Subject,Semester
 
 # Register your models here.
-admin.site.register(Department)
-admin.site.register(Subject)
 
-admin.site.register(Semester)
+
+
+
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('BranchName',)  # Fields to display in the list view
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('SubjectName',)  # Fields to display in the list view
+
+class SemAdmin(admin.ModelAdmin):
+    list_display = ('Semester',)  # Fields to display in the list view
+   
+
+admin.site.register(Department,DepartmentAdmin)
+admin.site.register(Subject,SubjectAdmin)
+
+admin.site.register(Semester,SemAdmin)
